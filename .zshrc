0e960f4adf03d367c5c9106c52ed4c963b04643f
MsFaceless <camilla.buys@gmail.com>
2020-02-03 13:48:21 +0200

    small changes

---
+++ .zshrc
@@ -0,0 +1,12 @@
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=999999
+SAVEHIST=999999
+bindkey -e
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/home/camilla/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
--- sync_to_nfs.sh
+++ sync_to_nfs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo mount -a
-rsync -varuzP /home/camilla/enviros /home/camilla/NFS/drive_2/Staff\ Folders/camilla
+rsync -varuzP /home/camilla/enviros /home/camilla/NFS/drive_1/staff_folders/camilla
 notify-send -u critical "Enviros backed up to NFS"
+rsync -varuzP /home/camilla/Documents /home/camilla/NFS/drive_1/staff_folders/camilla
+notify-send -u critical "Documents backed up to NFS"
