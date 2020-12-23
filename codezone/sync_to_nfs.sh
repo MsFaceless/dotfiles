72de5276c7bf53eea0b8b47b9df2eb9499a07a4d
Camilla <camilla.buys@gmail.com>
2020-12-23 08:28:52 +0200

    End of 2020 Commit

--- codezone/sync_to_nfs.sh
+++ codezone/sync_to_nfs.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+function dlswp {
+    find . -iname "*.pyc" -print0 -exec rm -r {} \;
+}
 
 sudo mount -a
-rsync -varuzP /home/camilla/enviros /home/camilla/NFS/drive_1/staff_folders/camilla
-notify-send -u critical "Enviros backed up to NFS"
-rsync -varuzP /home/camilla/Documents /home/camilla/NFS/drive_1/staff_folders/camilla
+
+#cd /home/camilla/enviros && dlswp
+#rsync -varuzzP /home/camilla/enviros /home/camilla/NFS/drive_1/staff_folders/camilla
+#notify-send -u critical "Enviros backed up to NFS"
+
+cd /home/camilla/Documents && dlswp
+rsync -varuzzP /home/camilla/Documents /home/camilla/NFS/drive_1/staff_folders/camilla
 notify-send -u critical "Documents backed up to NFS"
--- thedots/.profile
+++
@@ -1,5 +0,0 @@
-export EDITOR=/usr/bin/nano
-export QT_QPA_PLATFORMTHEME="qt5ct"
-export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
-
-export PATH="$HOME/.cargo/bin:$PATH"
